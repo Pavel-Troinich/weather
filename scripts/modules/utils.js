891a15f3d879087660219d323123e54f8d5d3826
Pavel <tr.pav.ar@gmail.com>
2023-06-25 17:20:37 +0300

    fix view

--- scripts/modules/utils.js
+++ scripts/modules/utils.js
@@ -40,7 +40,7 @@ export const getWeatherForecastData = (data) => {
     (item) =>
     new Date(item.dt_txt).getHours() === 12 && 
     new Date(item.dt_txt).getDate() >= new Date().getDate() &&
-    new Date(item.dt_txt).getDate() < new Date().getDate() + 4
+    new Date(item.dt_txt).getDate() < new Date().getDate() + 5
   );
 
   const forecastData = forecast.map((item) => {
